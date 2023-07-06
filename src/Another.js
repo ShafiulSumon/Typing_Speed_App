2a31adf6aa214b8263ef98ff9d6fe16f0d9584b3
Shafiul_Alam <shafiulahcb1007@gmail.com>
2023-07-06 12:43:21 +0600

    working on second

--- src/Another.js
+++ src/Another.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { generate, count } from 'random-words';
 import "bulma/css/bulma.min.css";
 
@@ -7,6 +7,7 @@ const SECONDS = 30;
 
 function Another() {
 
+    const [countDown, setCountDown] = useState(SECONDS);
     const [words, setWords] = useState([]);
     const [userInput, setUserInput] = useState("");
     const [currWordIndex, setCurrWordIndex] = useState(0);
@@ -14,9 +15,23 @@ function Another() {
     const [correctWords, setCorrectWords] = useState(0);
     const [incorrectWords, setIncorrectWords] = useState(0);
 
+    const startTimer = useRef(false);
+
     useEffect(() => {
         setWords(generateWords());
     }, []);
+
+    const runTimer = () => {
+        const interval = setInterval((value) => {
+            if(value > 0) {
+                return setCountDown(value - 1);
+            }
+            else {
+                clearInterval(interval);
+                return SECONDS;
+            }
+        }, 1000);
+    };
 
     const generateWords = () => {
         return new Array(NUMBER_OF_WORDS).fill(null).map(() => generate());
@@ -48,12 +63,22 @@ function Another() {
         }
     };
 
+    const refreshPage = () => {
+        window.location.reload();
+    };
+
     return (
         <div>
             {/* Header Text */}
             <div className="is-size-1 has-text-centered">
-                <h1>Typing-Test</h1>
+                <h1>Typing Test</h1>
             </div>
+
+            {/* Timer Section */}
+            <div className="is-size-2 has-text-centered has-text-danger">
+                <h2>{countDown}</h2>
+            </div>
+
 
             <div className="section">
                 <p>{words.map( (word, idx) => (
@@ -73,10 +98,39 @@ function Another() {
                     onKeyDown={handleKeyDown}
                 />
             </div>
+            
+            {/* Output Section */}
+            <div className="section has-text-centered">
+                <div className="columns">
+                    <div className="column">
+                        <p className="subtitle">Correct Words</p>
+                        <p className="is-size-3 subtitle">{correctWords}</p>
+                    </div>
+                    <div className="column">
+                        <p className="subtitle">Incorrect Words</p>
+                        <p className="is-size-3 subtitle">{incorrectWords}</p>
+                    </div>
+                    <div className="column">
+                        <p className="subtitle">Words per minute</p>
+                        <p className="is-size-3 subtitle">{correctWords*2}</p>
+                    </div>
 
-            <div>
-                <h2 className="subtitle">{correctWords}</h2>
-                <h2 className="subtitle">{incorrectWords}</h2>
+                    <div className="column">
+                        <p className="subtitle">Accuracy</p>
+                        <p className="is-size-3 subtitle">{Math.round((correctWords / (correctWords+incorrectWords)) * 100)}%</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className="has-text-centered">
+                <button 
+                    className="button has-background-warning" 
+                    onClick={refreshPage}
+                >
+                    Refresh
+                </button>
             </div>
 
         </div>
